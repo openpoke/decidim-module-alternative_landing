90785ea4b23db35300bb912f84f3a8f8813bd6a4
elviabth <ejbenedith@gmail.com>
2025-10-01 16:30:42 +0200

    refactor cell post method

--- app/cells/decidim/alternative_landing/content_blocks/latest_blog_posts_cell.rb
+++ app/cells/decidim/alternative_landing/content_blocks/latest_blog_posts_cell.rb
@@ -20,9 +20,16 @@ module Decidim
         end
 
         def posts
-          @posts ||= Blogs::Post.where(
-            component: component || components
-          ).limit(posts_to_show).order(created_at: :desc)
+          scope = Blogs::Post.where(component: component || components)
+
+          case model.settings.filter
+          when "organization"
+            scope = scope.where(decidim_author_type: "Decidim::Organization")
+          when "users"
+            scope = scope.where(decidim_author_type: ["Decidim::User", "Decidim::UserGroup"])
+          end
+
+          scope.limit(posts_to_show).order(created_at: :desc)
         end
 
         private
