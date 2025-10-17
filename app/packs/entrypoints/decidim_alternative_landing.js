c64f899dce92ba2d2e52dd1835e97833884f1341
Elvia Benedith <116598037+ElviaBth@users.noreply.github.com>
2025-10-17 10:58:15 +0200

    Add filter for meetings and posts (#2)
    * update form view
    
    * add post author select
    
    * customize sidebar content-block for posts
    
    * add meetings component select
    
    * add spec check
    
    * add meetings picker cell
    
    * fix meetings picker error
    
    * add JS file
    
    * refactor sidebar meetings picker
    
    * refactor meetings_picker
    
    * refactor selected_ids method
    
    * add dependent input JS file
    
    * change meeting_component_id type
    
    * refactor meetings method
    
    * fix spec
    
    * refactor base_cell
    
    * refactor sidebar methods
    
    * refactor latest_blog_posts content block

--- app/packs/entrypoints/decidim_alternative_landing.js
+++ app/packs/entrypoints/decidim_alternative_landing.js
@@ -1,4 +1,6 @@
 import "src/decidim/alternative_landing/alternative_landing.js"
+import "src/decidim/alternative_landing/choose_meetings"
+import "src/decidim/alternative_landing/meeting_dependent_input"
 
 require.context("../images", true)
 
